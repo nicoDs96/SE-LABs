f7544e98f6457d251b5bee19837e9607e5f7db5e
nicoDs96 <49313849+nicoDs96@users.noreply.github.com>
2019-07-02 12:35:05 +0200

    Add files via upload

---
+++ RESTwsCXF/RESTAPI/src/main/java/com/mycompany/restapi/Repository.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.restapi;
+
+import java.util.LinkedList;
+import java.util.List;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author Nodes
+ */
+@Path("risorse")
+@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+public class Repository {
+    private List<Risorsa> data = new LinkedList<>();
+
+    public Repository() {
+        Risorsa r = new Risorsa();
+        r.setId("uno");
+        r.setName("risorsa normale");
+        data.add(r);
+        
+        Risorsa r1 = new Risorsa();
+        r1.setId("due");
+        r1.setName("risorsa speciale");
+        data.add(r1);
+        
+        Risorsa r2 = new Risorsa();
+        r2.setId("tre");
+        r2.setName("tua madre a pecora");
+        data.add(r2);      
+    }
+    
+    @GET
+    @Path("{rid}")
+    @Produces(MediaType.APPLICATION_XML)
+    public Risorsa getRisorsa(@PathParam("rid") String id){
+        for(Risorsa r: this.data){
+            if(r.getId().equals(id) ){
+                System.out.print("return the result");
+                 return r;
+            }           
+        }
+        System.out.print("returning null.\nId passed:\t"+id);
+        return new Risorsa();
+    }
+    @POST
+    @Path("")
+    public Response addRisorsa(Risorsa r){
+        for(Risorsa res: this.data){
+            if(res.getId().equals( r.getId() ) ){ 
+                return Response.status(Response.Status.CONFLICT).build(); 
+            }          
+        }
+        this.data.add(r);
+        return Response.ok().build();
+    }
+    
+    @PUT
+    @Path("{rid}")
+    public Response updateRisorsa(@PathParam("rid") String id, Risorsa newRes){
+        for(Risorsa res: this.data){
+            if(res.getId().equals(id)  ){
+                if(res.equals(newRes) ){
+                    return Response.status(Response.Status.NOT_MODIFIED).build(); 
+                }
+                res.setId(newRes.getId());
+                res.setName(newRes.getName());
+                return Response.ok().build(); 
+            }          
+        }
+        return Response.status(Response.Status.NOT_FOUND).build();
+    }
+    
+    @DELETE
+    @Path("{rid}")
+    public Response deleteRisorsa(@PathParam("rid") String id){
+        for(Risorsa res: this.data){
+            if(res.getId().equals(id)  ){
+                this.data.remove(res);
+                return Response.ok().build(); 
+            }          
+        }
+        return Response.status(Response.Status.NOT_FOUND).build();
+    }
+    
+}
---
+++ RESTwsCXF/RESTAPI/src/main/java/com/mycompany/restapi/Server.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.restapi;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.ws.rs.core.MediaType;
+import org.apache.cxf.jaxrs.JAXRSServerFactoryBean;
+import org.apache.cxf.jaxrs.lifecycle.SingletonResourceProvider;
+import org.apache.cxf.jaxrs.provider.JAXBElementProvider;
+import org.codehaus.jackson.jaxrs.JacksonJsonProvider;
+
+/**
+ *
+ * @author Nodes
+ */
+public class Server {
+    public static void main(String[] args){
+        JAXRSServerFactoryBean factoryBean = new JAXRSServerFactoryBean();
+        factoryBean.setResourceClasses(Repository.class);
+        factoryBean.setResourceProvider(
+                new SingletonResourceProvider(new Repository())
+        );
+        
+        Map<Object, Object> extensionMappings = new HashMap<Object, Object>();
+        extensionMappings.put("xml", MediaType.APPLICATION_XML);
+        extensionMappings.put("json", MediaType.APPLICATION_JSON);
+        factoryBean.setExtensionMappings(extensionMappings);
+
+        List<Object> providers = new ArrayList<Object>();
+        providers.add(new JAXBElementProvider());
+        providers.add(new JacksonJsonProvider());
+        factoryBean.setProviders(providers);
+
+        
+        factoryBean.setAddress("http://localhost:8080/");
+        org.apache.cxf.endpoint.Server server = factoryBean.create();
+        System.out.println("Server up and running");
+    }
+}
