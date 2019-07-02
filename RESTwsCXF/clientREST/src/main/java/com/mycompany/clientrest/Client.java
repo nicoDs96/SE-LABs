0cd87f6e4e2bd1866eb2057da9f9c0a0af6298b3
nicoDs96 <49313849+nicoDs96@users.noreply.github.com>
2019-07-02 12:34:27 +0200

    Add files via upload

---
+++ RESTwsCXF/clientREST/src/main/java/com/mycompany/clientrest/Client.java
@@ -0,0 +1,131 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.clientrest;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.bind.JAXB;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.*;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.entity.InputStreamEntity;
+import org.apache.http.impl.client.*;
+
+/**
+ *
+ * @author Nodes
+ */
+public class Client {
+
+    private static String BASE_URL = "http://localhost:8080/risorse/";
+    private static CloseableHttpClient client;
+
+    public static void createClient() {
+        client = HttpClients.createDefault();
+    }
+
+    public static void closeClient() throws IOException {
+        client.close();
+    }
+
+    public static void main(String[] args) throws MalformedURLException, IOException, JAXBException {
+       /*
+        XML MESSAGES
+       */
+        
+        createClient();
+        
+        /*
+         GET
+         */
+        String nomeRisorsa = "tre";
+        String geturi = "http://localhost:8080/risorse/"+nomeRisorsa;
+        
+        HttpGet httpGet = new HttpGet(geturi);
+        httpGet.setHeader("Content-Type", "text/xml");
+        HttpResponse response = client.execute(httpGet);
+        System.out.print("\nGet effettuata.\nRisposta:\n" + response.toString()+"\nResponse Entity:\t"+response.getEntity().getContent().toString() );
+        Risorsa resFromGet = JAXB.unmarshal( response.getEntity().getContent(), Risorsa.class);
+        System.out.print("\n\nUnmarshall:\t"+resFromGet.toString()+"\n\n");
+        /*
+         POST
+         */
+        Risorsa oggettoRisorsa = new Risorsa();
+        oggettoRisorsa.setId("quattro");
+        oggettoRisorsa.setName("chinotto");
+        HttpPost httpPost = new HttpPost(BASE_URL  );
+        //creazione dummy file
+        JAXBContext jaxbContext = JAXBContext.newInstance(Risorsa.class);
+        Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+        jaxbMarshaller.marshal(oggettoRisorsa, new File("res.xml"));
+        File file = new File("res.xml");
+        InputStream targetStream = new FileInputStream(file);
+
+        httpPost.setEntity(new InputStreamEntity(targetStream)); //set the object as i
+
+        httpPost.setHeader("Content-Type", "text/xml");
+        response = client.execute(httpPost);
+        System.out.print("\nPost con risorsa:\n"+oggettoRisorsa.toString()+"\neseguita con esito:\t"+response.getStatusLine() );
+        
+        
+        /*
+         PUT
+        */
+        HttpPut httpPut = new HttpPut(BASE_URL + oggettoRisorsa.getId() ); 
+        
+        oggettoRisorsa.setName("NOME DI RISORSA");
+        jaxbContext = JAXBContext.newInstance(Risorsa.class);
+        jaxbMarshaller = jaxbContext.createMarshaller();
+        jaxbMarshaller.marshal(oggettoRisorsa, new File("res.xml"));
+        file = new File("res.xml");
+        targetStream = new FileInputStream(file);
+
+        httpPut.setEntity(new InputStreamEntity(targetStream));
+        httpPut.setHeader("Content-Type", "text/xml");
+        response = client.execute(httpPut);
+        System.out.print("\nPut con risorsa:\n"+oggettoRisorsa.toString()+"\neseguita con esito:\t"+response.getStatusLine() );
+        
+        /*
+         DELETE
+        */
+        HttpDelete httpDelete = new HttpDelete(BASE_URL + oggettoRisorsa.getId() );
+        response = client.execute(httpDelete);
+        System.out.print("\nDelete con risorsa:\n"+oggettoRisorsa.toString()+"\neseguita con esito:\t"+response.getStatusLine() );
+        
+        
+        /*
+         GET deleted element
+        */
+        geturi = "http://localhost:8080/risorse/"+oggettoRisorsa.getId();
+        
+        httpGet = new HttpGet(geturi);
+        httpGet.setHeader("Content-Type", "text/xml");
+        response = client.execute(httpGet);
+        if(response == null){
+           System.out.print("\nGet effettuata, risposta nulla.\nStato:\n" + response.getStatusLine() );
+        }else{
+            System.out.print("\nGet effettuata.\nClasse risposta:\n");
+        }
+        
+        
+        /*
+         JSON MESSAGES
+        */
+
+    }
+}
---
+++ RESTwsCXF/clientREST/src/main/java/com/mycompany/clientrest/Risorsa.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.clientrest;
+
+import java.util.Objects;
+import javax.xml.bind.annotation.*;
+
+/**
+ *
+ * @author Nodes
+ */
+@XmlRootElement(name="Risorsa")
+public class Risorsa {
+    private String id;
+    private String name;
+
+    public Risorsa() {
+        this.id = "";
+        this.name= "";
+    }
+
+    public String getId() {
+        return id;
+    }
+    @XmlElement(name="identifier")
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    @XmlElement(name="name")
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 71 * hash + Objects.hashCode(this.id);
+        hash = 71 * hash + Objects.hashCode(this.name);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Risorsa other = (Risorsa) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Risorsa{" + "id=\t" + id + ",\tname=\t" + name + "\t}";
+    }
+    
+    
+    
+    
+}
